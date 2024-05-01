71266a024c08977698ab83b4c8cf7a9efae2630e
zofiamilczarek <zmilcz@gmail.com>
2024-05-01 12:04:34 +0200

    created a baseline.py file where the baseline code will be

--- src/baseline.py
+++ src/baseline.py
@@ -1,1 +1,60 @@
 #this file contains the code for the baseline model
+
+from data.dataloader import preprocess_data, CFG
+import os
+from transformers import AutoTokenizer, AutoModelForTokenClassification, TrainingArguments, Trainer
+
+
+
+class CFG:
+    LABELS_LIST = ['B-NAME_STUDENT', 'B-EMAIL', 'B-USERNAME', 'B-ID_NUM', 'B-PHONE_NUM', 'B-URL_PERSONAL', 'B-STREET_ADDRESS', 'I-NAME_STUDENT', 'I-EMAIL', 'I-USERNAME', 'I-ID_NUM', 'I-PHONE_NUM','I-URL_PERSONAL','I-STREET_ADDRESS', 'O']
+    #BERT model
+    model_name = 'bert-base-uncased'
+    #batch size
+    batch_size = 8
+    #number of epochs
+    epochs = 5
+    #learning rate
+    lr = 1e-5
+
+    seed = 42
+
+    label2id = {label: i for i, label in enumerate(LABELS_LIST)}
+    label2id['[PAD]'] = -100
+    id2label = {i: label for label, i in label2id.items()}
+
+    __local_path = os.path.abspath(os.path.dirname(__file__))
+    __target_dir = os.path.join(__local_path,'..','models', 'baseline')
+
+    training_args = TrainingArguments(
+        output_dir=__target_dir, 
+        evaluation_strategy="epoch"
+        )
+
+
+
+
+def train_model(data_train, data_eval, model, training_args):
+    """
+    Trains the model
+    """
+    trainer = Trainer(
+        model=model,                         
+        args=CFG.training_args,                  
+        train_dataset=data_train,         
+        eval_dataset=data_eval             
+    )
+
+    trainer.train()
+
+    return True
+
+
+if __name__ == '__main__':
+    local_path = os.path.abspath(os.path.dirname(__file__))
+    data_path = os.path.join(local_path, '../data/raw/synthetic/mixtral.json')
+
+    tokenizer = AutoTokenizer.from_pretrained(CFG.model_name)
+    model = AutoModelForTokenClassification.from_pretrained(CFG.model_name, num_labels=len(CFG.id2label), id2label=CFG.id2label, label2id=CFG.label2id)
+    data = preprocess_data(data_path, tokenizer, overlap_size=0)
+    print(data)
